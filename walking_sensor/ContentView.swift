0414d01d6c03d652e8adbf1946932902a50ede88
Tatsuya Mizuguchi <mizuguchi_yho@yahoo.co.jp>
2022-12-29 13:40:50 +0900

    Initial Commit

---
+++ walking_sensor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  walking_sensor
+//
+//  Created by Tatsuya Mizuguchi on 2022/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ walking_sensor/walking_sensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  walking_sensorApp.swift
+//  walking_sensor
+//
+//  Created by Tatsuya Mizuguchi on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct walking_sensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
